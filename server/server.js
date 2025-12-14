5584a2194e236e4bf0fa9a27c10700d97d202718
Nnamdi <imcraylo@gmail.com>
2025-12-14 22:58:30 +0000

    "bugfix: dbconnection complete"

--- server/server.js
+++ server/server.js
@@ -5,9 +5,18 @@ import express from "express";
 
 import cors from "cors";
 
-import { db } from "./dbConnection";
+import { db } from "./server/dbConnection.js";
+
+const app = express();
 
 //configs
+app.use(cors());
+app.use(express.json());
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.info(`Server is running in port ${PORT}`);
+});
 
 //port
 
