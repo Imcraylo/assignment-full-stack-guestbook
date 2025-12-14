58af74562fc02d54272d240e7ac34d05c21192ea
Nnamdi <imcraylo@gmail.com>
2025-12-14 20:13:44 +0000

    chore: add template code

---
+++ client/src/app.js
@@ -0,0 +1,23 @@
+//TODO: collect users' data and send to the server
+
+//submit event to collect users' data
+
+//fetch the POST server route
+
+// fetch("url", {
+//    method:,
+//    header:, {
+
+//    },
+//    body: JSON.stringify
+// })
+
+//! Once you finish your project, replace your locathost url with the deployed server url from Render
+
+//================================================================================================================
+
+//TODO: render users' data on the interface
+
+//fetch the GET route from the server
+
+// render the data using DOM elements (one per piece of data)
